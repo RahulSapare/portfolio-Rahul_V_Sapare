682434ee654b7258fd0325e5f614ec8d33b5e911
RahulSapare <sapare.0207@gmail.com>
2021-01-03 17:35:18 +0530

    project Setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+   return (
+      <div className="App">
+         <p>Hello World !</p>
+          <button className="btn btn-success"> subscribe </button>
+      </div>
+   );
+}
+export default App;
