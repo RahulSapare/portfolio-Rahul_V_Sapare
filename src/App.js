fb7424e69250caa8592a9205dc56331269b54ed2
RahulSapare <sapare.0207@gmail.com>
2021-01-03 22:55:05 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Navbar from "./components/Navbar";
 
 
 function App() {
    return (
       <div className="App">
-         <p>Hello World !</p>
-          <button className="btn btn-success"> subscribe </button>
+         <Navbar/>
       </div>
    );
 }
